608316fc09f44816ce1fd622ac8448cbdca149b0
anuchin <abhijith.nuchin@gmail.com>
2017-07-10 21:59:44 +0530

    Final touches

--- trailbot.py
+++ trailbot.py
@@ -3,7 +3,7 @@ import config
 import time
 
 reddit = praw.Reddit(user_agent="Rules bot by u/Zero-Kelvin",
-                    client_id="",
+                    client_id=config.client_id,
                     client_secret=config.client_secret,
                     username=config.username,
                     password=config.password)
@@ -15,7 +15,7 @@ phrases_to_match = ["what is rule 1?","what is rule 1","what is rule #1?","what 
                     "what are rules 1 and 2?","what are rules 1 and 2",
                     "what is rules 1 and 2?","what is rules 1 and 2?"]
 
-reply_comment="Rule 1. Be Attractive \n\n Rule 2. Be Unactrattive"
+reply_comment="Rule 1: Be Attractive \n\n Rule 2: Be Unactrattive \n\n =============================================\n\n Bot by u/Zero-Kelvin."
 
 subreddit=reddit.subreddit("ZeroKelvin")
 cache=[]
@@ -25,11 +25,11 @@ print(reddit.user.me())
 
 def reply_bot():
     print("entered the reply fucntion")
-    comments = subreddit.comments(limit=100)
+    comments = subreddit.comments(limit=400)
     print("fetched comments")
+    print("Checking phrases")
     for comment in comments:
         comment_text = comment.body.lower()
-        print("Checking phrases")
         for phrase in phrases_to_match:
 
             if phrase in comment_text and comment.id not in cache:
