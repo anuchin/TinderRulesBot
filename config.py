3d18aacf547e01a0b921aa56bb58b0184d6a5d4a
anuchin <abhijith.nuchin@gmail.com>
2017-07-11 14:25:50 +0530

    Final

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username=""
-password=""
-client_id=""
-client_secret=""
+username="Tinder-Rules-Bot"
+password="beattractive"
+client_id="oTv4H7FQ6-GHQQ"
+client_secret="nnuRorTQAiLxvkTRRighINjua2c"
